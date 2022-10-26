058268dd876de98611f49359123abe14ce248129
Aditya Bhardwaj <abd.aditya10@outlook.com>
2022-10-26 10:20:08 +0530

    .

--- OOP/26-10/2.cpp
+++ OOP/26-10/2.cpp
@@ -1,25 +1,26 @@
-//WAP to throw and handle "Array of bound" exception
-#include<iostream>
-#include<stdexcept>
+// WAP to throw and handle "Array of bound" exception
+#include <iostream>
+#include <stdexcept>
 using namespace std;
 int main()
 {
-    int arr[5] = {1,2,3,4,5};
-    cout<<"Enter index to accessed: ";
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << "Enter index to accessed: ";
     int i;
-    cin>>i;
+    cin >> i;
     int elt;
-    if(i < 0 || i > 4)
+    if (i < 0 || i > 4)
     {
         throw runtime_error("Index cannot be accessed.");
     }
-    try 
+    try
     {
         elt = arr[i];
-        cout<<"The element at index "<<i<<": "<<elt;
+        cout << "The element at index " << i << ": " << elt;
     }
-    catch(runtime_error& e)
+    catch (runtime_error &e)
     {
-        cout<<"Exception occured"<<endl<<e.what();
+        cout << "Exception occured" << endl
+             << e.what();
     }
 }
